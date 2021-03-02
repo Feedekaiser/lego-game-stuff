1c2f301c08af7fdad45b04aad5fbb9140947be5f
Feedekaiser <77873991+Feedekaiser@users.noreply.github.com>
2021-03-02 16:04:50 +0800

    Update ToBase.lua

--- Number/ToBase.lua
+++ Number/ToBase.lua
@@ -1,6 +1,6 @@
 local Characters = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'g', 'h', 'i', 'j', 'k', 'l'}
 
-return function(Number,) 
+return function(Number, Base) 
     local Negative = Number < 0
     if Number < Base then return tostring(Number) end
 
