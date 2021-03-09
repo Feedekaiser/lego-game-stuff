1fdbbe486598ce4cf543277971368de99ead1c07
Feedekaiser <77873991+Feedekaiser@users.noreply.github.com>
2021-03-09 11:10:45 +0800

    Update ToBase.lua

--- Number/ToBase.lua
+++ Number/ToBase.lua
@@ -1,19 +1,46 @@
-local Characters = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'g', 'h', 'i', 'j', 'k', 'l'}
+local tostring = tostring
+local floor    = math.floor
+
+local Characters = {
+	[ 0] = "0";
+	[ 1] = "1";
+	[ 2] = "2";
+	[ 3] = "3";
+	[ 4] = "4";
+	[ 5] = "5";
+	[ 6] = "6";
+	[ 7] = "7";
+	[ 8] = "8";
+	[ 9] = "9";
+	[10] = "A";
+	[11] = "B";
+	[12] = "C";
+	[13] = "D";
+	[14] = "E";
+	[15] = "F";
+	[16] = "G";
+	[17] = "H";
+	[18] = "I";
+	[19] = "J";
+	[20] = "K";
+	[21] = "L";
+	[22] = "M";
+	[23] = "N";
+}
 
 return function(Number, Base) 
     local Negative = Number < 0
+
+	Number = abs(Number)
+
     if Number < Base then return tostring(Number) end
 
-    Number = math.abs(Number)
-
     local Result = ""
-    while Number > 0 do
-        Result = Characters[math.floor(Number % Base) + 1] .. Result
-        Number = math.floor(Number / Base)
-    end
 
-    if Negative then
-        Result = "-" .. Result
-    end
-    return Result
+    repeat
+        Result = Characters[floor(Number % Base)] .. Result
+        Number = floor(Number / Base)
+    until Number == 0
+
+    return Negative and "-" .. Result or Result
 end
