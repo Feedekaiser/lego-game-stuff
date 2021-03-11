dc8ed9b92f7cb3f15eab3c0232ad1222b899c41b
Feedekaiser <77873991+Feedekaiser@users.noreply.github.com>
2021-03-11 21:28:03 +0800

    Update stringify.lua

--- stringify.lua
+++ stringify.lua
@@ -6,18 +6,18 @@ stringify = function(v,spaces, usesemicolon, depth)
 	elseif not next(v) then
 		return '{}'
 	end
-	
+
 	spaces = spaces or 4
 	depth = depth or 1
-	
+
 	local space = (" "):rep(depth*spaces)
 	local sep = usesemicolon and ";" or ","
 	local s = "{"
-	
+
 	for k, x in next, v do
 		s = s..("\n%s[%s] = %s%s"):format(space,type(k)=='number'and tostring(k)or('"%s"'):format(tostring(k)), stringify(x, spaces, usesemicolon, depth+1), sep)
 	end
-	
+
 	return ("%s\n%s}"):format(s:sub(1,-2), space:sub(1, -spaces-1))
 end
 
